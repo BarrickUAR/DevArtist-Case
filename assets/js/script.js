86cab48e541a80bf0137ec04b9a7f137ca0adfae
Berk Uçar <berkucar0901@gmail.com>
2025-02-09 16:43:09 +0300

    Hover Fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const counters = document.querySelectorAll(".counter");
 counters.forEach((counter) => {
   let target = +counter.getAttribute("data-target");
   let count = 0;
-  let increment = target / 200; // 
+  let increment = target / 200;
 
   let updateCount = () => {
     count += increment;
